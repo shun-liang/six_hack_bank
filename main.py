ade7683b11e5869202d90b33d300ffdfbd296c9b
Shun Liang <shun.liang.public@gmail.com>
2016-03-05 12:47:23 +0000

    Added API end points for card creation.

--- main.py
+++ main.py
@@ -19,28 +19,42 @@ def link_account():
     '''
     if request.method == 'POST':
         request_json = request.get_json(force=True)
-        username = request_json['user_name']
-        account_number = request_json['account_number']
-        sort_code = request_json['sort_code']
+        username = request_json['username']
+        account_number = request_json['accountnumber']
+        sort_code = request_json['sortcode']
         add_user(username, account_number, sort_code)
-        print get_users()
-        return 'Success'
+        #print get_users()
+        return ''
+
+@app.route('/card', methods = ['PUT'])
+def add_card():
+    '''
+    Add a new card.
+    '''
+    request_json = request.get_json(force=True)
+    username = request_json['username']
+    card_alias = request_json['cardalias']
+    card_number = request_json['cardnumber']
+    user = get_user(username)
+    user.add_card(card_alias, card_number)
+    return ''
+
 
 @app.route('/card', methods=['POST'])
 def change_card_state():
-    if request.method == 'POST':
-        request_json = request.get_json(force=True)
-        username = request_json['username']
-        card_alias = request_json['cardalias']
-        action = request_json['action']
-        if action == 'lock':
-            # Card locking logic
-            #card_number = 1234123412341234
-            user = get_user(username)
-            user.lock_card(card_alias)
-            return ''
+    '''
+    Modify the state of a card.
+    '''
+    request_json = request.get_json(force=True)
+    username = request_json['username']
+    card_alias = request_json['cardalias']
+    action = request_json['action']
+    if action == 'lock':
+        # Card locking logic
+        #card_number = 1234123412341234
+        user = get_user(username)
+        user.lock_card(card_alias)
+        return ''
 
 if __name__ == "__main__":
-    create_test_data() 
-    print get_users()
     app.run(debug=True)
